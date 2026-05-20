69b2451a89d2464114901c6463e4e77871f82522
JamesDavis <jamespdavis25@outlook.com>
2026-05-20 18:23:12 +0100

    Adding support for retrieving LocationData

---
+++ portfolio-blazor-v2/portfolio-blazor-v2/LocationData.cs
@@ -0,0 +1,11 @@
+﻿namespace portfolio_blazor_v2
+{
+    public class LocationData
+    {
+        public double Latitiude { get; set; }
+        public double Longitude { get; set; }
+        public double Accuracy { get; set; }
+        public double? Altitude { get; set; }
+        public string Timestamp { get; set; } = string.Empty;
+    }
+}
